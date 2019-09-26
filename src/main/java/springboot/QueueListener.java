43750e673f4412990ce7c18565515c930eac776d
chong.zi.work.note.book <wangchenmlg@163.com>
2019-09-26 16:07:43 +0800

    add the base activeMq function

---
+++ src/main/java/springboot/QueueListener.java
@@ -0,0 +1,16 @@
+package springboot;
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Component;
+
+@Component
+public class QueueListener {
+
+    @JmsListener(destination = "publish.queue", containerFactory = "jmsListenerContainerQueue")
+    @SendTo("out.queue")
+    public String receive(String text){
+        System.out.println("QueueListener: consumer-a 收到一条信息: " + text);
+        return "consumer-a received : " + text;
+    }
+}
